d3a94f5d82216c040c83bbeb40964156c2be7aef
russellknox84 <russell.knox@digital.homeoffice.gov.uk>
2017-02-19 21:19:56 +0000

    added automation app, js and css script and es6 babel

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const express = require("express");
-const hbs = require("express-handlebars");
+import express from 'express';
+import hbs from "express-handlebars";
+
+import routes from './app/routes'
 
 const app = express();
 
-app.engine("handlebars", hbs({defaultLayout: "main"}))
+app.engine("handlebars", hbs({
+    extname: '.handlebars',
+    layoutsDir: __dirname + "/views",
+    defaultLayout: __dirname + "/views/layouts/main"
+}))
 app.set("view engine", "handlebars")
 
-app.get("/", (req, res) => res.render("home"))
+routes(app);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log("listening on PORT: ", PORT));
