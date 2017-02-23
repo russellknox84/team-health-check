24c13aa52020b8b0994747e97ca3f934b30a50d4
russellknox84 <russell.knox@digital.homeoffice.gov.uk>
2017-02-23 09:12:31 +0000

    added es6 support for tests, an example test file and .babelrc file

---
+++ assets/js/index.js
@@ -0,0 +1,3 @@
+const name = "Home Office Digital"
+
+export default name;
--- test/test.js
+++ test/test.js
@@ -1,7 +1,12 @@
 const { expect } = require("chai");
 
+import name from '../assets/js'
+
 describe("test test", () => {
     it("should return true", () => {
         expect(1).to.equal(1)
+    })
+    it("should return 'Home Office Digital'", () => {
+        expect(name).to.equal("Home Office Digital")
     })
 })
