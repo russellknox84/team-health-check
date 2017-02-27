d3e688638318e956b123e86721c75fcda3f49c97
Lucas Cairns <lucascairns@HOD207.local>
2017-02-27 15:58:51 +0000

    Updates to module as per unit test specs.

--- app/models/validation.js
+++ app/models/validation.js
@@ -1,16 +1,28 @@
 // Functions for validation middleware
 
+const validator = require('the-vladiator');
+
 // DONE: Create unit test for module.
-// TODO: Consider using vladiator to chain validation and improve reliability of checks, module can be used Client/Server side.
+// DONE: Consider using vladiator to chain validation and improve reliability of checks, module can be used Client/Server side.
 // TODO: Create meaningful errors.
 // TODO: Refactor code to use a callback pattern inline with existing NodeJS patterns.
 
 var validation = {
     radioButton: function(data) {
         // Radio Button validation code goes here.
+        var result = validator(data).isRequired().notEmpty().isNumber().lowerThan(6).higherThan(0).didPass();
+        return result;
     },
     textArea: function(data) {
         // Text Area validation code goes here
+        var result1 = validator(data).isRequired().notEmpty().isString().shorterThan(65).didPass();
+        var result2 = data.split(" ").length > 3
+                      ? false
+                      : true;
+        var specChars = /[\<\>!@#\$%^&\*,]+/;
+        var result3 = !specChars.test(data)
+        if(result1 && result2 && result3) return true;
+        else return false;
     }
 }
 
@@ -19,10 +31,10 @@ var validation = {
 //      validation.check('validation rule', 'data');
 
 validation.check = function (name, data) {
-    if(validation.hasOwnProperty(name)){
+    if(validation.hasOwnProperty(name) && data){
         return validation[name] && validation[name].apply(validation, [].slice.call(arguments, 1));
     } else {
-        console.log("validation rule for " + name + " does not exist");
+        return false;
     }
 };
 
--- test/validationTest.js
+++ test/validationTest.js
@@ -1,28 +1,57 @@
 const validation = require('../app/models/validation');
+const should = require('chai').should();
 
 
-describe("Validation Module", () => {
+describe('Validation Module', () => {
 
-    describe("Module testing", () => {
-        it("should process the correct validation when supported command provided");
-        it("should fail when no data is passed");
-        it("should fail when supplied incorrect command");
+    describe('Module testing', () => {
+        it('should process the correct validation when supported command provided', () => {
+            validation.check('radioButton', 2).should.equal(true); 
+        });
+        it('should fail when no data is passed', () => {
+            validation.check('radioButton').should.equal(false);
+        });
+        it('should fail when supplied incorrect command', () => {
+            validation.check('cake', 2).should.equal(false); 
+        });
     });
 
-    describe("Radio Button Validation", () => {
-        it("should pass if the value is defined, a number and in range");
-        it("should fail if the value is undefined");
-        it("should fail if the value is not a number");
-        it("should fail if the value is lower than the range");
-        it("should fail if the value is higher than the range");
+    describe('Radio Button Validation', () => {
+        it('should pass if the value is defined, a number and in range', () => {
+            validation.check('radioButton', 4).should.equal(true);
+        });
+        it('should fail if the value is undefined', () => {
+            validation.check('radioButton').should.equal(false);
+        });
+        it('should fail if the value is not a number', () => {
+            validation.check('radioButton', 'cake').should.equal(false);
+        });
+        it('should fail if the value is lower than the range', () => {
+            validation.check('radioButton', -20).should.equal(false);
+        });
+        it('should fail if the value is higher than the range', () => {
+            validation.check('radioButton', 10).should.equal(false);
+        });
     });
 
-    describe("Text Button Validation", () => {
-        it("should pass if the value is defined, a string and in range");
-        it("should fail when the value is undefined");
-        it("should fail when the value is zero length");
-        it("should fail when the value has illegal characters");
-        it("should fail when the value is higher than specified wordcount");
-        it("should fail when the value is higher than specified character count");
+    describe('Text Button Validation', () => {
+        it('should pass if the value is defined, a string and in range', () => {
+            validation.check('textArea', 'cat dog bird').should.equal(true);
+        });
+        it('should fail when the value is undefined', () => {
+            validation.check('textArea').should.equal(false);
+        });
+        it('should fail when the value is zero length', () => {
+            validation.check('textArea', '').should.equal(false);
+        });
+        it('should fail when the value has illegal characters', () => {
+            validation.check('textArea', '</;$').should.equal(false);
+        });
+        it('should fail when the value is higher than specified wordcount', () => {
+            validation.check('textArea', 'cat dog bird horse').should.equal(false);
+        });
+        it('should fail when the value is higher than specified character count', () => {
+            validation.check('textArea', 'mQpKPmDGVDybmoOpgkLVOYtCVLAHYCYlUnxlzbRWgSXKLYUIMjPyVmnvFrqStzQVyDr').should.equal(false);
+        });
     });
 })
