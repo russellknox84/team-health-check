b76c5bd2a68adff819a26bb775f55f0d3ee3149a
russellknox84 <russell.knox@digital.homeoffice.gov.uk>
2017-03-03 12:55:22 +0000

    added helper functions and app.post for success route

--- assets/js/health-form.js
+++ assets/js/health-form.js
@@ -1,46 +1,28 @@
 import axios from 'axios'
-import validation from "../../app/models/validation"
+import { validateUserInput, removeChild, appendErrorMessageElement, addEventListener } from "./helpers"
 
-const removeChild = (parent, child) => parent.querySelector(`.${child}`).removeChild(parent.querySelector(`.errormessage${child}`))
+const healthFormSubmission = () => {
 
-const appendErrorMessageElement = (parent, nodes) => {
-    const a = document.createElement('div')
-    a.textContent = 'A input value must be given..'
-    a.style.color = 'red'
-    a.className = `errormessage${nodes.id}`
-    return parent.querySelector(`.${nodes.id}`).appendChild(a)
-}
+    const submitHealthForm = (ev) => {
+    ev.preventDefault()
 
-const validateInputs = (nodes, fieldsets) => {
-    //remove error messgae if already exists
-    if(fieldsets.querySelector(`.errormessage${nodes.id}`)) removeChild(fieldsets, nodes.id)
-    //console.log(validation.check("textarea", 's'), "textare....")
-    //!validation.check(nodes.type, fieldsets[nodes.id].value)
-    if (fieldsets[nodes.id].value === '') { 
-        appendErrorMessageElement(fieldsets, nodes)
-        return false
-    }
-    //returns true if value from input is found 
-    return true
-}
+    const userInputResponses = ev.target
+    const healthCheckQuestions = healthCheck["health-check"].questions
 
-const healthForm = document.querySelector(".radioNameForm") 
-    .addEventListener('submit', (e) => {
-        e.preventDefault()
+    const userInput = healthCheckQuestions
+        .map(question => validateUserInput(question, userInputResponses[question.id].value))
 
-        const fieldsets = e.target
-        const healthCheckData = healthCheck["health-check"].questions
-
-        const truthify = healthCheckData
-            .map(nodes => validateInputs(nodes, fieldsets))
-            .every(a => a)
+    if (userInput.every(response => response.userResponse)) {
+        const userResponse = userInput.map(response => Object.assign(response, {userResponse: userInputResponses[response.id].value}))
+        return axios.post("/user-data", { userResponse }) 
+            .then(a => window.location.href = "/sucess") 
+    } else userInput
+        .map(response => removeChild(response, userInputResponses))
+        .filter(response => !response.userResponse)
+        .map(question => appendErrorMessageElement(question, userInputResponses))
    
-        if (!truthify) return
+} 
+    addEventListener(".health-check", "submit", submitHealthForm)
+}
 
-        const userData = healthCheckData
-            .map(nodes => Object.assign(nodes, {userInput: fieldsets[nodes.id].value}))
-     
-        //send data
-        axios.post("user-data", { userData })
- 
-    })  
+export default healthFormSubmission
