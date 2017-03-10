dcf2d0e177ddc133069ec35fcdd096d7f6e84e9a
Lucas Cairns <lucascairns@HOD207.local>
2017-03-10 15:54:56 +0000

    Fixes to Mongoose config

--- app/mongo/config.js
+++ app/mongo/config.js
@@ -1,12 +1,13 @@
 import db from "mongoose";
 
-const DB_HOST = process.env.DB_HOST || 'mongodb://localhost/teamHealthCheck';
-const DB_USER = process.env.DB_USER;
-const DB_PASS = process.env.DB_PASS;
-var DB_LOGIN = '';
-if(DB_USER && DB_PASS) DB_LOGIN = `${DB_USER}:${DB_PASS}@`;
+const DB_HOST = process.env.DB_HOST || 'localhost/teamHealthCheck';
+const DB_USER = process.env.DB_USER || false;
+const DB_PASS = process.env.DB_PASS || false;
+var uri;
+if(DB_USER && DB_PASS) uri = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}`;
+else uri = `mongodb://${DB_HOST}`;
 
-db.connect(`mongodb://${DB_LOGIN}${DB_HOST}`, (err, cb) => {
+db.connect(uri, (err, cb) => {
     console.log('connected')
 })
 
