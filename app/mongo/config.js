8367f4ee3bf34c01b6efbf341634d8e77cd30ce6
russellknox84 <russell.knox@digital.homeoffice.gov.uk>
2017-03-10 15:01:47 +0000

    mlab config

--- app/mongo/config.js
+++ app/mongo/config.js
@@ -1,5 +1,12 @@
 import db from "mongoose"
 
+const DB_HOST = process.env.DB_HOST || 'mongodb://localhost/teamHealthCheck';
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const CREDS = `${DB_USER}:${DB_PASS}`;
+
+const URL = `mongodb://localhost/teamHealthCheck'`;
+
 db.connect('mongodb://localhost/teamHealthCheck', (err, cb) => {
     console.log('connected')
 })
