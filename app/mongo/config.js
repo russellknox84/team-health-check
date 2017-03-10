dd1de01b6347f5a29901f5e1d5d5cd6b7357ed09
Lucas Cairns <lucascairns@HOD207.local>
2017-03-10 15:25:05 +0000

    Changes to Mongoose config to include remote host

--- app/mongo/config.js
+++ app/mongo/config.js
@@ -1,14 +1,13 @@
-import db from "mongoose"
+import db from "mongoose";
 
 const DB_HOST = process.env.DB_HOST || 'mongodb://localhost/teamHealthCheck';
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
-const CREDS = `${DB_USER}:${DB_PASS}`;
+var DB_LOGIN = '';
+if(DB_USER && DB_PASS) DB_LOGIN = `${DB_USER}:${DB_PASS}@`;
 
-const URL = `mongodb://localhost/teamHealthCheck'`;
-
-db.connect('mongodb://localhost/teamHealthCheck', (err, cb) => {
+db.connect(`mongodb://${DB_LOGIN}${DB_HOST}`, (err, cb) => {
     console.log('connected')
 })
 
-export default db
+export default db;
