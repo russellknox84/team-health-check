939e8cdbb380a808a4057e8fb74e14d3ed8acd1c
Lucas Cairns <lucascairns@HOD207.local>
2017-04-07 09:28:43 +0100

    Change to app.js

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ var hbs = exphbs.create({
 });
 
 app.use(express.static("./dist/public"))
+app.use("/:Project", express.static("./dist/public"))
 app.engine("handlebars", hbs.engine)
 app.set("view engine", "handlebars")
 
